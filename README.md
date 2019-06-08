683120d52b2ef8e59a00e9c374cc7209e631ee50
Alan Herculano Diniz <alanhdiniz@pop-os.localdomain>
2019-06-07 23:42:43 -0300

    Update readme and fixing output filenames

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # LP-Trab3-HASKELL
 Grouping problem solution with the leader algorithm in Haskell.
+
+Compilation: ghc main.hs leader.hs reader.hs points.hs
+
+Execution: ./main "points filename" "distance filename"
--- main.hs
+++ main.hs
@@ -14,11 +14,11 @@ import Reader
 
 main :: IO()
 main = do
-    args <- getArgs
-    pointsStr <- readFile $ head args
-    distStr <- readFile $ head $ tail args
-    let (points, limit) = parseInput pointsStr distStr
-    let (sse, groups) = calculateResults points limit
-    let (sseStr, groupsStr) = createOutput sse groups
-    writeFile "results.txt" groupsStr
-    writeFile "saida.txt" sseStr
+    args <- getArgs -- Getting the command line arguments
+    pointsStr <- readFile $ head args -- Opening and reading the points file
+    distStr <- readFile $ head $ tail args -- Opening and reading the distance file
+    let (points, limit) = parseInput pointsStr distStr -- Parsing the strings to get the data
+    let (sse, groups) = calculateResults points limit -- Executing the grouping algorithm
+    let (sseStr, groupsStr) = createOutput sse groups -- Creating the output strings
+    writeFile "saida.txt" groupsStr -- Points output file
+    writeFile "result.txt" sseStr -- Distance output file
