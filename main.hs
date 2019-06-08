7caaa11e211a2922ed315df5b12204c5a988c031
Alan Herculano Diniz <alanhdiniz@pop-os.localdomain>
2019-06-08 10:44:32 -0300

    Getting default inputs if there are no files given

--- main.hs
+++ main.hs
@@ -15,8 +15,8 @@ import Reader
 main :: IO()
 main = do
     args <- getArgs -- Getting the command line arguments
-    pointsStr <- readFile $ head args -- Opening and reading the points file
-    distStr <- readFile $ head $ tail args -- Opening and reading the distance file
+    pointsStr <- if not $ null args then readFile $ head args else readFile "entrada.txt" -- Opening and reading the points file
+    distStr <- if not $ null args then readFile $ head $ tail args else readFile "distancia.txt" -- Opening and reading the distance file
     let (points, limit) = parseInput pointsStr distStr -- Parsing the strings to get the data
     let (sse, groups) = calculateResults points limit -- Executing the grouping algorithm
     let (sseStr, groupsStr) = createOutput sse groups -- Creating the output strings
