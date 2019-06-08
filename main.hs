5116bf3176ead04d46714e1ee4d98fb244305527
Alan Herculano Diniz <alanhdiniz@pop-os.localdomain>
2019-06-07 22:19:43 -0300

    Fixing erros, IO and update .gitignore

--- main.hs
+++ main.hs
@@ -17,8 +17,8 @@ main = do
     args <- getArgs
     pointsStr <- readFile $ head args
     distStr <- readFile $ head $ tail args
-    -- let (points, limit) = parseInput pointsStr distStr
+    let (points, limit) = parseInput pointsStr distStr
     let (sse, groups) = calculateResults [[0,1],[1,2],[2,5]] 2
-    -- let (sseStr, groupsStr) = createOutput (sse, groups)
-    writeFile "results.txt" (show groups ++ "\n")
-    writeFile "saida.txt" (show sse ++ "\n")
+    let (sseStr, groupsStr) = createOutput (sse, groups)
+    writeFile "results.txt" groupsStr
+    writeFile "saida.txt" sseStr
--- reader.hs
+++ reader.hs
@@ -13,9 +13,17 @@ module Reader(
  createOutput
 ) where
 
-parseInput :: String -> String -> ([[Double]], Double)
-parseInput pointsStr distStr = undefined
+readDouble :: String -> Double
+readDouble = read
 
+parseInput :: String -> String -> ([[Double]], Double)
+parseInput pointsStr distStr = (points, limit)
+    where
+        treated = map words $ lines pointsStr
+        points = [[readDouble j | j <- i] | i <- treated]
+        limit = read distStr :: Double
 
 createOutput :: (Double, [[Int]]) -> (String, String)
-createOutput (sse, groups) = undefined
+createOutput (sse, groups) = (show sse ++ "\n", groupsStr)
+    where
+        groupsStr = unlines [unwords [show (groups!!i!!j) | j <- [0 .. length (groups!!i) - 1]] | i <- [0 .. length groups - 1]]
